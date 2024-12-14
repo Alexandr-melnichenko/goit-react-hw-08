a397ac9cdfde17ec9ab3b110cdfb59946ce2b77f
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2024-12-14 15:32:05 +0200

    fix contacts

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,16 @@
+import { useDispatch } from "react-redux";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import s from "./ContactsPage.module.css";
+import { useEffect } from "react";
+import { fetchContacts } from "../../redux/contacts/operations";
 
 const ContactsPage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <div className={s.wrap}>
       <h1>Phonebook</h1>
